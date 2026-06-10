2c3fe787b1bb955de7e9df29522768d8b53449bf
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-10 18:28:56 +0530

    feat: navbar for phone

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -56,6 +56,23 @@ const Navbar = () => {
         />
       </div>
       {/* Sidebar for small screen */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img src={assets.dropdown_icon} alt="" className="h-4 rotate-180" />
+            <p>Back</p>
+          </div>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border-b' to="/">HOME</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border-b' to="/collection">COLLECTION</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border-b' to="/about">ABOUT</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border-b' to="/contact">CONTACT</NavLink>
+        </div>
+      </div>
     </div>
   );
 };
