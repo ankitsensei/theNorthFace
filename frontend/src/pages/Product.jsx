c898897225dd7c331aaca11215fed9c564c004b1
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-14 03:10:38 +0530

    feat: Related products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -34,6 +35,7 @@ const Product = () => {
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
             {productData.image.map((item, index) => (
               <img
+                key={index}
                 src={item}
                 alt=""
                 onClick={() => setImage(item)}
@@ -116,6 +118,11 @@ const Product = () => {
           </p>
         </div>
       </div>
+      {/* Display related products */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
