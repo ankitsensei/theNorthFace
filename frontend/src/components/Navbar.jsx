957ddd5c1502b0dfb5189ca570e0dc5d4eef03e6
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-13 08:33:17 +0530

    feat: collegtion page: search filter

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets.js";
 import { NavLink, Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext.jsx";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const { setShowSearch } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
         <img src={assets.logo} className="w-36" alt="" />
       </Link>
-      <ul className="hidden sm:flex gap-5 text-sm text-gray-700"> 
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
@@ -29,7 +31,12 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+        />
         <div className="group relative">
           <img
             src={assets.profile_icon}
--- frontend/src/context/ShopContextProvider.jsx
+++ frontend/src/context/ShopContextProvider.jsx
@@ -1,11 +1,21 @@
 import { ShopContext } from "./ShopContext";
 import { products } from "../assets/frontend_assets/assets";
+import { useState } from "react";
 
 const ShopContextProvider = ({ children }) => {
+  const currency = "₹";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+
   const value = {
     products,
-    currency: "₹",
-    delivery_fee: 10,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
