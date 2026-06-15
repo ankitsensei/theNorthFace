e0c28ccbb27db3e0520d9548a262eba566d036a5
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-15 11:57:31 +0530

    feat: contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2x1 pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img src={assets.contact_img} alt="" className="w-full md:max-w-120" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            749728 Railway Station <br /> Delhi
+          </p>
+          <p className="text-gray-500">
+            Tel: (425) 342-1029 <br />
+            Email: admin@forever.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about teams and job openings
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
+};
+
+export default Contact;
