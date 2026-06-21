308859a1d8a90a8456a82c1cc1f9111ffc6d6c24
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-21 09:05:30 +0530

    fix: after submit, form clear

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import axios from "axios";
 import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Add = ({token}) => {
+const Add = ({ token }) => {
   const [image1, setImage1] = useState(false);
   const [image2, setImage2] = useState(false);
   const [image3, setImage3] = useState(false);
@@ -36,11 +37,25 @@ const Add = ({token}) => {
 
       const response = await axios.post(
         backendUrl + "/api/product/add",
-        formData, {headers: {token}}
+        formData,
+        { headers: { token } },
       );
-      console.log(response.data);
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setName("");
+        setDescription("");
+        setImage1(false);
+        setImage2(false);
+        setImage3(false);
+        setImage4(false);
+        setPrice("");
+      } else {
+        toast.error(response.data.message);
+      }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
