604c506579d55a771d9745854bdd52721cefa3b3
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-21 16:17:46 +0530

    feat: now products in frontend will be fetched from database instead hardcoded file

--- frontend/src/context/ShopContextProvider.jsx
+++ frontend/src/context/ShopContextProvider.jsx
@@ -1,15 +1,17 @@
 import { ShopContext } from "./ShopContext";
-import { products } from "../assets/frontend_assets/assets";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const ShopContextProvider = ({ children }) => {
   const currency = "₹";
   const delivery_fee = 10;
+  const backendurl = import.meta.env.VITE_BACKEND_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
@@ -72,6 +74,25 @@ const ShopContextProvider = ({ children }) => {
     return totalAmount;
   };
 
+  const getProductData = async () => {
+    try {
+      const response = await axios.get(backendurl + "/api/product/list");
+      // console.log(response.data);
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
   const value = {
     products,
     currency,
@@ -86,6 +107,7 @@ const ShopContextProvider = ({ children }) => {
     updateQuantity,
     getCartAmount,
     navigate,
+    backendurl,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
