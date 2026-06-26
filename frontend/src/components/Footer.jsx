1a48495fdd200bd291554431e8b5398885e61e47
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-06-26 11:30:37 +0530

    feat: added admin portal in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/frontend_assets/assets";
 const Footer = () => {
   return (
     <div>
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[2fr_1fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
           <img src={assets.logo} alt="" className="mb-5 w-32" />
           <p className="w-full md:w-2/3 text-gray-600">
@@ -28,6 +28,14 @@ const Footer = () => {
             <li>+91 9988998899</li>
             <li>contact@thenorthface.com</li>
           </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">Admin Login</p>
+          <a href="https://the-north-face-admin.vercel.app/" target="_blank">
+            <button className="border px-8 py-3 bg-black text-white">
+              Admin Portal
+            </button>
+          </a>
         </div>
       </div>
       <div>
